ca8d767c9733e900ab1c9034b8e7b769cbabdb5e
Yanis <y.bezzeghoud@gmail.com>
2023-09-28 09:20:51 +0200

    1 commit

--- app.js
+++ app.js
@@ -1,48 +1,109 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// ========== Importation des modules nécessaires ==========
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Express est un cadre minimal et flexible pour les applications web Node.js
+const express = require('express');
 
+// bodyParser est un middleware qui analyse les corps de requête entrants dans un middleware
+const bodyParser = require('body-parser');
+
+// cookieParser est un middleware qui analyse les cookies attachés à l'objet de requête (req)
+const cookieParser = require('cookie-parser');
+
+// express-session est un middleware de gestion de session pour Express
+const session = require('express-session');
+
+// jwt (JSON Web Token) pour gérer l'authentification basée sur des tokens
+const jwt = require('jsonwebtoken');
+
+// path fournit des utilitaires pour travailler avec les chemins de fichiers et de répertoires
+const path = require('path');
+
+// twig est un moteur de template pour JavaScript
 const twig = require('twig');
 
-var app = express();
+// helmet aide à sécuriser les applications Express en définissant divers en-têtes HTTP
+const helmet = require('helmet');
 
-const {connectDB} = require ('./config/database');
+// rateLimit est un middleware pour limiter le taux de requêtes répétées à l'API
+const rateLimit = require('express-rate-limit');
 
-// Conexion à la base de données
-connectDB();
+// Pour charger les variables d'environnement à partir d'un fichier .env
+require('dotenv').config();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'twig');
+// Importation des routes et des configurations spécifiques à notre application
+const userRoutes = require('./routes/userRoutes');
+const { initDatabase } = require('./config/database');
+const { isLoggedIn } = require('./middlewares/auth');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// ========== Configuration et initialisation de l'application ==========
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Initialisation de l'application Express
+const app = express();
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Initialisation de la base de données
+initDatabase();
+
+// Configuration de la sécurité
+app.use(helmet()); // Protège l'application de certaines vulnérabilités bien connues
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100 // limite chaque IP à 100 requêtes par fenêtre
+}));
+
+// Configuration des middlewares
+app.use(bodyParser.urlencoded({ extended: true })); // Analyse les corps des requêtes en format x-www-form-urlencoded
+app.use(bodyParser.json()); // Analyse les corps des requêtes en format JSON
+app.use(cookieParser()); // Parse les cookies et popule req.cookies
+app.use(session({
+    secret: process.env.JWT_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+app.use(express.static(path.join(__dirname, 'public'))); // Sert les fichiers statiques du répertoire 'public'
+
+// Configuration des messages flash
+app.use((req, res, next) => {
+    res.locals.flashMessages = req.session.flashMessages;
+    delete req.session.flashMessages;
+    next();
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Configuration de Twig comme moteur de rendu
+app.set('view engine', 'twig');
+app.set('views', __dirname + '/views');
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Fonction pour gérer les actifs dans Twig
+twig.extendFunction('asset', function(path) {
+    return '/public' + path;
+});
+
+// Middleware pour vérifier si l'utilisateur est authentifié
+app.use((req, res, next) => {
+    const token = req.cookies.token;
+    if (token) {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedUser) => {
+            if (!err && decodedUser) {
+                req.user = decodedUser;
+            }
+        });
+    }
+    next();
+});
+
+// Routes de l'application
+app.use('/', userRoutes);
+app.get('/', (req, res) => {
+    res.render('home', { user: req.user });
+});
+
+// Middleware pour gérer les erreurs
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+app.use((req, res, next) => {
+    res.status(404).render('404');
 });
 
 module.exports = app;
--- config/database.js
+++ config/database.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const MONGODB_URI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
+// Configuration de la connexion à la base de données en utilisant les variables d'environnement
+const MONGODB_URI = process.env.MONGODB_URI;
 
 /**
  * Fonction pour établir la connexion à la base de données.
  */
-const connectDB = async () => {
+const connectToDatabase = async () => {
     try {
-        await mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`, {
+            useNewUrlParser: true, // Pour utiliser le nouveau système de connexion à la base de données
+            useUnifiedTopology: true, // Pour utiliser le nouveau moteur de découverte et de surveillance du serveur
         });
         console.log('La connexion à la base de données a été établie avec succès.');
     } catch (error) {
@@ -18,4 +19,25 @@ const connectDB = async () => {
     }
 };
 
-module.exports = { connectDB };
+/**
+ * Fonction pour déconnecter de la base de données.
+ */
+const disconnectFromDatabase = async () => {
+    try {
+        await mongoose.disconnect();
+        console.log('Déconnexion de la base de données réussie.');
+    } catch (error) {
+        console.error('Erreur lors de la déconnexion de la base de données:', error);
+    }
+};
+
+/**
+ * Fonction pour initialiser la base de données.
+ * Cette fonction établit simplement la connexion.
+ */
+const initDatabase = async () => {
+    await connectToDatabase();
+};
+
+// Exportation des fonctions pour utilisation ailleurs dans l'application
+module.exports = { initDatabase, disconnectFromDatabase };
